d030193a367ed7c6425af544d49949bd8d08cead
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-01-27 19:26:59 +0300

    pagination in routes.php was fixed

--- router/routes.php
+++ router/routes.php
@@ -4,6 +4,6 @@ use App\Controllers\CustomersController;
 use App\Controllers\HomeController;
 
 return [
-      '/' => ['controller' => HomeController::class, 'action' => 'index'],
-      '/customer' => ['controller' => CustomersController::class, 'action' => 'index']
+    '/' => ['controller' => HomeController::class, 'action' => 'index'],
+    '/customer' => ['controller' => CustomersController::class, 'action' => 'index']
 ];
